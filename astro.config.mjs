fdfadcbdbac801ca0d3b59765318162b7d3af01e
Diego Manuel Jaeger Rapoport <diegomrapoport@gmail.com>
2026-07-04 22:19:53 -0300

    feat: add svelte for client side fun

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), svelte()],
   site: 'https://github.com/RapoporTech/rapoportech-blog'
 });
